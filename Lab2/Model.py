d9bea14a1f35fe81bad5dc02b4acb4f3da2f0160
Veronika <54397786+Santo020v@users.noreply.github.com>
2022-12-16 23:19:01 +0200

    Update model.py

--- Lab2/Model.py
+++ Lab2/Model.py
@@ -44,23 +44,42 @@ class Model:
     def printResult(self):
         print('-----RESULTS-----')
 
-        global_max_observed_queue_length = 0
-        global_mean_queue_length_accumulator = 0
-        global_failure_probability_accumulator = 0
-        global_mean_load_accumulator = 0
+        meanqueue_length_sum = 0
+        failure_probability_sum = 0
+        meanload_sum = 0
         num_of_processors = 0
 
         for e in self.list:
             e.printResult()
             if isinstance(e, Process.Process):
                 num_of_processors += 1
+
                 mean_queue_length = e.mean_queue / self.tcurr
-
                 failure_probability = e.failure / (e.quantity + e.failure) if (e.quantity + e.failure) != 0 else 0
-
                 mean_load = e.mean_load / self.tcurr
 
                 print(f"Average queue length: {mean_queue_length}")
                 print(f"Failure probability: {failure_probability}")
-                #print(f"Avarage load: {mean_load}")
+                print(f"Avarage load: {mean_load}")
                 print(' ')
+
+                meanqueue_length_sum += mean_queue_length
+                failure_probability_sum += failure_probability
+                meanload_sum += mean_load
+
+        meanqueue_length_result = mean_queue_length / num_of_processors
+        failure_probability_result = failure_probability / num_of_processors
+        meanload_result = mean_load / num_of_processors
+
+        print(f"Global average queue length: {meanqueue_length_result}")
+        print(f"Global failure probability: {failure_probability_result}")
+        print(f"Global average load: {meanload_result}")
+
+        print()
+
+        return {
+            "meanqueue_length_result": meanqueue_length_result,
+            "failure_probability_result": failure_probability_result,
+            "meanload_result": meanload_result
+        }
+    
